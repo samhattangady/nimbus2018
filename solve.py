c498763579c292c31f3f8313378e6ea215afe561
Samarth Hattangady <samhattangady@gmail.com>
2018-03-26 10:57:37 +0530

    Reduce unecessary clicks

--- solve.py
+++ solve.py
@@ -5,31 +5,30 @@ import time
 import pyautogui
 from PIL import Image
 
-# TILES = {
-            # None: Image.open('images/unclicked.png'),
-            # 0: Image.open('images/zero.png'),
-            # 1: Image.open('images/one.png'),
-            # 2: Image.open('images/two.png'),
-            # 3: Image.open('images/three.png'),
-            # 4: Image.open('images/four.png'),
-            # 5: Image.open('images/five.png'),
-            # 6: Image.open('images/six.png'),
-            # 7: Image.open('images/seven.png'),
-            # 8: Image.open('images/eight.png')
-        # }
 TILES = {
-            None: 'images/unclicked.png',
-            0: 'images/zero.png',
-            1: 'images/one.png',
-            2: 'images/two.png',
-            3: 'images/three.png',
-            4: 'images/four.png',
-            5: 'images/five.png',
-            6: 'images/six.png',
-            7: 'images/seven.png',
-            8: 'images/eight.png'
+            None: Image.open('images/unclicked.png'),
+            0: Image.open('images/zero.png'),
+            1: Image.open('images/one.png'),
+            2: Image.open('images/two.png'),
+            3: Image.open('images/three.png'),
+            4: Image.open('images/four.png'),
+            5: Image.open('images/five.png'),
+            6: Image.open('images/six.png'),
+            7: Image.open('images/seven.png'),
+            8: Image.open('images/eight.png')
         }
-
+# TILES = {
+            # None: 'images/unclicked.png',
+            # 0: 'images/zero.png',
+            # 1: 'images/one.png',
+            # 2: 'images/two.png',
+            # 3: 'images/three.png',
+            # 4: 'images/four.png',
+            # 5: 'images/five.png',
+            # 6: 'images/six.png',
+            # 7: 'images/seven.png',
+            # 8: 'images/eight.png'
+        # }
 
 def restart():
     pyautogui.press('f2')
@@ -87,7 +86,7 @@ def get_board():
     tiles = sorted(tiles, key=lambda x: (x[1], x[0]))
     height = sum(1 for i in tiles if i[0]==tiles[0][0])
     width = sum(1 for i in tiles if i[1]==tiles[0][1])
-    board = {i: {'location': tile, 'neighbours': get_neighbours(i, width, height), 'value': None} for i, tile in enumerate(tiles)}
+    board = {i: {'location': tile, 'neighbours': get_neighbours(i, width, height), 'value': None, 'action': None} for i, tile in enumerate(tiles)}
     return board
 
 def read_tile_value(screen, tile):
@@ -112,7 +111,18 @@ def click_tile_and_read(tile_index, board):
     x, y, w, h = tile['location']
     pyautogui.moveTo(x+w/2, y+h/2)
     pyautogui.click()
-    return read_full_board(board)
+    screen = pyautogui.screenshot()
+    read_tile_value(screen, tile)
+    if tile['value'] == 0:
+        # more than one tile was uncovered
+        return read_full_board(board)
+    return board
+
+def click_tile(tile_index, board):
+    tile = board[tile_index]
+    x, y, w, h = tile['location']
+    pyautogui.moveTo(x+w/2, y+h/2, 0)
+    pyautogui.click()
 
 def is_evaluable(tile_index, board):
     tile = board[tile_index]
@@ -128,17 +138,20 @@ def evaluate(tile_index, board):
     mines = len([t for t in tile['neighbours'] if board[t]['value'] == 'mine'])
     if value == mines:
        for t in unclicked:
-           board = click_tile_and_read(t, board)
+           # board = click_tile_and_read(t, board)
+           board[t]['action'] = 'reveal'
+           # click_tile(t, board)
     if value-mines == len(unclicked):
        for t in unclicked:
-           flag_tile(t, board)
+           # flag_tile(t, board)
+           board[t]['action'] = 'flag'
            board[t]['value'] = 'mine'
     return board
 
 def flag_tile(tile_index, board):
     tile = board[tile_index]
     x, y, w, h = tile['location']
-    pyautogui.moveTo(x+w/2, y+h/2)
+    pyautogui.moveTo(x+w/2, y+h/2, 0)
     pyautogui.press('space')
 
 def print_board(board):
@@ -156,18 +169,40 @@ def print_tile(tile, board):
     for n in tile['neighbours']:
         print(board[n]['value'], end=' ')
 
-if __name__ == '__main__':
+def sweep():
     board = get_board()
     # pick a random tile and start the game
     tile = random.choice(list(board.keys()))
-    board = click_tile_and_read(tile, board)
+    click_tile(tile, board)
+    # board = click_tile_and_read(tile, board)
     while True:
-        start_board = board
         # generate a list of all the tiles that should be evaluated
+        board = read_full_board(board)
         evaluable = [tile for tile in board if is_evaluable(tile, board)]
         for tile in evaluable:
             board = evaluate(tile, board)
+        to_click = []
+        to_flag = []
+        for tile in board:
+            if board[tile]['action'] == 'reveal':
+                board[tile]['action'] = None
+                to_click.append(tile)
+            if board[tile]['action'] == 'flag':
+                board[tile]['action'] = None
+                to_flag.append(tile)
+        for tile in to_click:
+            click_tile(tile, board)
+        for tile in to_flag:
+            flag_tile(tile, board)
         if len(evaluable) == 0: 
             print('winner winner chicken dinner')
             break
+        if not to_click and not to_flag:
+            restart()
+            board = get_board()
+            tile = random.choice(list(board.keys()))
+            click_tile(tile, board)
+
+if __name__ == '__main__':
+    sweep()
 
